4b63d66412a2e5e80527883562f5c5a22399f3a4
DanyarMamusov <mamdanram2008@gmail.com>
2026-05-14 06:27:40 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from db_manager import get_connection
+from database.db_manager import get_connection
 class User:
     def __init__(self, id=None, login=None, password=None, role=None):
         self.id = id
